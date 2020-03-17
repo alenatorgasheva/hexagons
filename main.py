68ea9cac1b49ef8333a91db09a8b2a0628c54694
alenatorgasheva <in-fa@mail.ru>
2020-03-17 18:59:17 +0700

    START!

---
+++ main.py
@@ -0,0 +1,50 @@
+# Case - study
+
+# This program
+
+# Developers : Daniel A.         (%),
+#              Zemtseva A.       (%),
+#              Torgasheva A.     (%).
+
+# Tasks:
+#       НАСТЯ
+#   - меню: цвет (get_color_choice), количество 6-угольников (get_num_hexagons) !!! Учесть возможность ошибки ввода
+#   - локализация
+#
+#       АЛИНА
+#   - нарисовать один шестиугольник, размещение шестигольников - в одной функции draw_hexagon
+#
+#       АЛЁНА
+#   - вычислить длинну стороны шестиугольника, а также величину d (см на картинке у Минака с формулами)
+#   - оформление программы (код ревью, комменты и т.д.)
+
+
+
+from turtle import *
+
+
+def get_num_hexagons():
+    return
+
+
+def get_color_choice():
+    return
+
+
+def side_length():
+    return
+
+
+def d_length():
+    return
+
+
+def draw_hexagon(x, y, side_len, color):
+    pass
+
+
+def main():
+    pass
+
+
+main()
