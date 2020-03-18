1de54bd85ca479c8a43b49647f15e0104a1b15a2
AlinaDaniel <alina.daniel66@icloud.com>
2020-03-18 17:26:36 +0300

    All work with turtle is done.

--- main.py
+++ main.py
@@ -19,17 +19,17 @@
 #   - оформление программы (код ревью, комменты и т.д.)
 
 
-
-from turtle import *
+import turtle as t
 import math
 
 
 def get_num_hexagons():
-    return 1
+    hexagon_number = int(input())
+    return hexagon_number
 
 
 def get_color_choice():
-    return
+    return ('pink', 'lavender')
 
 
 def side_length(d):
@@ -43,13 +43,48 @@ def d_length(n):
 
 
 def draw_hexagon(x, y, side_len, color):
-    pass
+    t.up()
+    t.goto(x, y)
+    t.down()
+    t.right(30)
+    t.fillcolor(color)
+    t.begin_fill()
+    for i in range(6):
+        t.forward(side_len)
+        t.right(60)
+    t.left(30)
+    t.end_fill()
+
+
+def draw_row(x, y, d, s, number, color_1, color_2):
+    for i in range(number):
+        if i % 2 == 0:
+            color = color_1
+        else:
+            color = color_2
+        draw_hexagon(x + d / 2, y, s, color)
+        x += d
+
+
+def fill_space(x, y, d, s, number, color_1, color_2, ptr):
+    for i in range(number):
+        if ptr == 2:
+            ptr = 0
+            color_1, color_2 = color_2, color_1
+        if i % 2 == 1:
+            draw_row(x + d / 2, y - s * 1.5 * i, d, s, number, color_1, color_2)
+            ptr += 1
+        else:
+            draw_row(x, y - s * 1.5 * i, d, s, number, color_1, color_2)
+            ptr += 1
 
 
 def main():
     hexagon_number = get_num_hexagons()
-    d = d_length(hexagon_number)
-    side = side_length(d)
+    t.speed(10.5)
+    color_1,color_2 = get_color_choice()
+    fill_space(-250, 250, d_length(hexagon_number), side_length(d_length(hexagon_number)), hexagon_number, color_1, color_2, 1)
+    t.done()
 
 
 main()
